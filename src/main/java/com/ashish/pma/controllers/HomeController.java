08eda0f5d94bd8c93fbe87e0a31a737af6b6cf61
Aashish Ambre <a.ambre06@gmail.com>
2020-10-17 17:05:31 +0530

    Added an environment variable envVersionNum

--- src/main/java/com/ashish/pma/controllers/HomeController.java
+++ src/main/java/com/ashish/pma/controllers/HomeController.java
@@ -9,6 +9,7 @@ import com.ashish.pma.entities.Project;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    @Value("${version}")
+    private String version;
+
     @Autowired
     ProjectRepository projectRepository;
 
@@ -28,6 +32,8 @@ public class HomeController {
 
     @GetMapping("/")
     public String displayHome(Model model) throws JsonProcessingException {
+
+        model.addAttribute("version", version);
 
         Map<String, Object> map = new HashMap<>();
 
